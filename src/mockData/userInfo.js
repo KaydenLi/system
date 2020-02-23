1dd6e204e6e70d55da8711f7b3b6153814ec49de
kaydenli <kaydenli@qq.com>
2020-02-23 14:44:41 +0800

    使用mock数据，暂时代替网络请求数据

---
+++ src/mockData/userInfo.js
@@ -0,0 +1,55 @@
+export default {
+    _id: "820807451",
+    adminFlag: false,
+    loginStatus: true,
+    welcomeFlag: true,
+    userName: "KaydenLi",
+    address: "湖北省武汉市洪山区珞喻路1037号华中科技大学",
+    phone: "12345678901",
+    email: "kaydenli@qq.com",
+    projectsCount: 0,
+    viewablesCount: 0,
+    avatar:"https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+    projects: [{
+        projectName: "测试1",
+        date: "2019-09-09",
+        address: "wuhan",
+        _id: "1"
+    },
+    {
+        projectName: "测试2",
+        date: "2019-09-09",
+        address: "wuhan",
+        _id: "2"
+    },
+    {
+        projectName: "测试3",
+        date: "2019-09-09",
+        address: "wuhan",
+        _id: "3"
+    },
+    {
+        projectName: "测试4",
+        date: "2019-09-09",
+        address: "wuhan",
+        _id: "4"
+    },
+    {
+        projectName: "测试5",
+        date: "2019-09-09",
+        address: "wuhan",
+        _id: "5"
+    }],
+    viewables: [{
+        projectName: "测试",
+        date: "2019-09-09",
+        address: "wuhan",
+        _id: "111"
+    }, {
+        projectName: "测试",
+        date: "2019-09-09",
+        address: "wuhan",
+        _id: "222"
+    }
+    ],
+}
