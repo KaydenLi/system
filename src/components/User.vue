2e8511e0fb8220846d31919de2052151e0ccc75c
kaydenli <kaydenli@qq.com>
2020-02-23 14:43:24 +0800

    增加用户资料修改页面

---
+++ src/components/User.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="user-container">
+    <div class="user-info">
+      <div class="left">
+        <div class="avatar">
+          <img :src="userInfo.avatar" alt="头像" />
+        </div>
+        <p>
+          <i class="el-icon-user"></i>
+          &nbsp;{{userInfo.userName}}
+        </p>
+        <p>
+          <i class="el-icon-message"></i>
+          &nbsp;{{userInfo.email}}
+        </p>
+        <p>
+          <i class="el-icon-mobile-phone"></i>
+          &nbsp;{{userInfo.phone}}
+        </p>
+      </div>
+      <div class="right">
+        <table>
+          <tr>
+            <th class="title">ID</th>
+            <td>
+              <i class="el-icon-house small-screen-show"></i>
+              {{userInfo._id}}
+            </td>
+          </tr>
+          <tr>
+            <th class="title">用户权限</th>
+            <td>
+              <i class="el-icon-s-check small-screen-show"></i>
+              {{userType}}
+            </td>
+          </tr>
+          <tr>
+            <th class="title">用户住址</th>
+            <td>
+              <i class="el-icon-place small-screen-show"></i>
+              {{userInfo.address}}
+            </td>
+          </tr>
+          <tr>
+            <th class="title">项目总数</th>
+            <td>
+              <i class="el-icon-medal-1 small-screen-show"></i>
+              {{userInfo.projectsCount}}个我的项目
+            </td>
+          </tr>
+          <tr>
+            <th class="title">可查看项目</th>
+            <td>
+              <i class="el-icon-medal small-screen-show"></i>
+              {{userInfo.viewablesCount}}个可查看项目
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    userInfo: null
+  },
+  computed: {
+    userType() {
+      return this.userInfo.adminFlag == true ? "管理员" : "普通用户";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.user-container {
+  display: inline-block;
+  text-align: center;
+}
+.user-info {
+  padding: 20px;
+  border: 1px solid #e4e7ed;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+.left {
+  height: 100%;
+  text-align: center;
+  padding: 20px;
+}
+.left p i {
+  color: #409eff;
+}
+.right {
+  padding-left: 20px;
+  border-left: 1px solid #ebeef5;
+}
+.avatar img {
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+}
+.title {
+  height: 3em;
+  line-height: 3em;
+  text-align: right;
+  padding-right: 30px;
+}
+.small-screen-show {
+  display: none;
+}
+@media screen and (max-width: 769px) {
+  .avatar img {
+    width: 50px;
+    height: 50px;
+  }
+  .title {
+    display: none;
+  }
+  .left {
+    padding: 0;
+    margin: 0;
+  }
+  .right {
+    border: none;
+    padding: 0;
+    margin: 0;
+  }
+  .small-screen-show {
+    display: inline-block;
+    color: #409eff;
+  }
+}
+</style>>
