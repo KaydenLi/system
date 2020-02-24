8fdaf51cfb9562d844f46c28ca387826a169928a
kaydenli <kaydenli@qq.com>
2020-02-24 21:05:43 +0800

    增加了申请授权、授权及授权历史页面

--- src/views/home/EditInfo.vue
+++ src/views/home/EditInfo.vue
@@ -1,22 +1,197 @@
 <template>
   <div>
     <siteHeader :header="header"></siteHeader>
+    <el-main>
+      <el-row>
+        <el-col
+          class="border"
+          :xs="{span: 24, offset: 0}"
+          :sm="{span: 18, offset: 3}"
+          :md="{span: 14, offset: 5}"
+          :lg="{span: 10, offset: 7}"
+          :xl="{span: 8, offset: 8}"
+        >
+          <el-form
+            :model="model"
+            :rules="rules"
+            ref="rules"
+            status-icon
+            label-width="80px"
+            class="demo-ruleForm"
+            label-position="right"
+          >
+            <el-form-item label="用户名" prop="userName">
+              <el-input v-model="model.userName" clearable></el-input>
+            </el-form-item>
+            <el-form-item label="修改头像">
+              <el-upload
+                class="avatar-uploader"
+                :action="$http.defaults.baseURL+'/upload/avatars'"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+              >
+                <img v-if="model.avatar" :src="model.avatar" class="avatar el-upload" />
+                <i v-else class="el-icon-plus avatar-uploader-icon el-upload"></i>
+              </el-upload>
+            </el-form-item>
+            <el-form-item label="电话号码" prop="phone">
+              <el-input v-model="model.phone" clearable :disabled="disabledChangePhone">
+                <el-button slot="append" @click="changePhone">更换手机号</el-button>
+              </el-input>
+            </el-form-item>
+            <el-form-item v-if="showVerifyCode" label="验证码" prop="verificationCode">
+              <el-input v-model="model.verificationCode" clearable>
+                <el-button slot="append" @click="getVerificationCode">发送验证码</el-button>
+              </el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+              <el-input v-model="model.email" clearable></el-input>
+            </el-form-item>
+            <el-form-item label="地址" prop="address">
+              <el-input v-model="model.address" clearable></el-input>
+            </el-form-item>
+            <el-form-item size="large">
+              <el-button type="primary" @click="(editUser('rules'))">修改</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </el-main>
+    <siteFooter></siteFooter>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   data() {
     return {
+      showVerifyCode: false,
+      disabledChangePhone: true,
+      model: {
+        avatar: "",
+        userName: "",
+        phone: "",
+        email: "",
+        address: "",
+        verificationCode: ""
+      },
       header: {
         menu: "编辑资料",
         toPageName: "返回主页",
         to: "/"
+      },
+      getVerificationCode() {
+        alert("获取验证码");
+      },
+      rules: {
+        userName: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 3,
+            max: 20,
+            message: "长度在 3 到 20 个字符",
+            trigger: "change"
+          }
+        ],
+        phone: [
+          { required: true, message: "请输入电话号码", trigger: "blur" },
+          { min: 11, max: 11, message: "长度为11个字符", trigger: "change" }
+        ],
+        verificationCode: [
+          { required: true, message: "请输入验证码", trigger: "change" },
+          { min: 4, max: 4, message: "长度为4个字符", trigger: "change" }
+        ],
+        email: [
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"]
+          }
+        ],
+        address: [
+          { required: true, message: "请输入项目所在地", trigger: "blur" },
+          {
+            min: 6,
+            max: 50,
+            message: "长度在 6 到 50 个字符",
+            trigger: "change"
+          }
+        ]
       }
     };
+  },
+  computed: {
+    ...mapState(["userInfo"])
+  },
+  methods: {
+    changePhone() {
+      this.disabledChangePhone = false;
+      this.showVerifyCode = true;
+    },
+    editUser(rules) {
+      this.$refs[rules].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          return false;
+        }
+      });
+      //TODO：修改信息
+      //TODO：修改state.userInfo
+    },
+    handleAvatarSuccess() {
+      this.$message.info(`头像上传成功`);
+      window.console.log(this.model.file);
+    }
+  },
+  mounted() {
+    this.model.avatar = this.userInfo.avatar;
+    this.model.userName = this.userInfo.userName;
+    this.model.phone = this.userInfo.phone;
+    this.model.email = this.userInfo.email;
+    this.model.address = this.userInfo.address;
   }
 };
 </script>
 
 <style scoped>
+.border {
+  border: 1px solid #dcdfe6;
+  border-radius: 5px;
+  padding: 30px;
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 100px;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+}
+.avatar {
+  width: 100px;
+  height: 100px;
+  display: block;
+}
+@media screen and (max-width: 769px) {
+  .border {
+    border: none;
+    box-shadow: none;
+  }
+  .demo-ruleForm {
+    margin: 0;
+    padding: 0;
+  }
+}
 </style>
--- src/views/home/Root.vue
+++ src/views/home/Root.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- header部分 -->
     <el-header>
       <div class="nav">
         <span class="logo">结构三维可视化监测系统&nbsp;</span>
@@ -29,13 +30,17 @@
             ></i>
           </div>
           <el-tabs type="border-card" value="first" style="min-height:450px;">
+            <!-- 我的项目标签页 -->
             <el-tab-pane label="我的项目" name="first">
               <Header :header="projectsHeader"></Header>
-              <el-table stripe :data="userInfo.projects">
+              <el-table stripe :data="userInfo.projects" :empty-text="emptyText.myprojects">
                 <el-table-column type="index" label="序号" :index="1" width="100px"></el-table-column>
-                <el-table-column prop="projectName" label="项目名称"></el-table-column>
-                <el-table-column prop="date" label="创建日期"></el-table-column>
-                <el-table-column prop="address" label="项目地址"></el-table-column>
+                <el-table-column
+                  v-for="item in projrctsTable"
+                  :key="item.projectName"
+                  :prop="item.prop"
+                  :label="item.label"
+                ></el-table-column>
                 <el-table-column label="操作" fixed="right">
                   <template slot-scope="scope">
                     <el-button type="primary" size="mini" @click="toDetail(scope.row._id)">详情</el-button>
@@ -43,13 +48,37 @@
                 </el-table-column>
               </el-table>
             </el-tab-pane>
-            <el-tab-pane label="可查看项目" name="second">
-              <Header :header="viewableHeader"></Header>
-              <el-table stripe :data="userInfo.viewables">
+
+            <!-- 开放项目标签页 -->
+            <el-tab-pane label="开放项目" name="second">
+              <Header :header="requestHeader"></Header>
+              <el-table stripe :data="authAboutProjects.toCheck" :empty-text="emptyText.requests">
                 <el-table-column type="index" label="序号" :index="1" width="100px"></el-table-column>
-                <el-table-column prop="projectName" label="项目名称"></el-table-column>
-                <el-table-column prop="_id" label="项目ID"></el-table-column>
-                <el-table-column prop="address" label="项目地址"></el-table-column>
+                <el-table-column
+                  v-for="item in toAuthTable"
+                  :key="item.projectName"
+                  :prop="item.prop"
+                  :label="item.label"
+                ></el-table-column>
+                <el-table-column label="操作" fixed="right">
+                  <template slot-scope="scope">
+                    <el-button
+                      type="primary"
+                      size="mini"
+                      @click="toApplicationDetail(scope.row._id)"
+                    >操作</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <Header :header="viewableHeader" class="top-margin"></Header>
+              <el-table stripe :data="authAboutProjects.toCheck" :empty-text="emptyText.viewables">
+                <el-table-column type="index" label="序号" :index="1" width="100px"></el-table-column>
+                <el-table-column
+                  v-for="item in viewablesTable"
+                  :key="item.projectName"
+                  :prop="item.prop"
+                  :label="item.label"
+                ></el-table-column>
                 <el-table-column label="操作" fixed="right">
                   <template slot-scope="scope">
                     <el-button
@@ -61,7 +90,43 @@
                 </el-table-column>
               </el-table>
             </el-tab-pane>
-            <el-tab-pane label="用户中心" name="third">
+
+            <!-- 授权项目标签页 -->
+            <el-tab-pane label="授权项目" name="third">
+              <Header :header="openRequestHeader"></Header>
+              <el-table stripe :data="authAboutProjects.toCheck" :empty-text="emptyText.checks">
+                <el-table-column type="index" label="序号" :index="1" width="100px"></el-table-column>
+                <el-table-column
+                  v-for="item in authTable"
+                  :key="item.projectName"
+                  :prop="item.prop"
+                  :label="item.label"
+                ></el-table-column>
+                <el-table-column label="操作" fixed="right">
+                  <template slot-scope="scope">
+                    <el-button type="primary" size="mini" @click="toAuthDetail(scope.row._id)">操作</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <Header :header="openHeader" class="top-margin"></Header>
+              <el-table stripe :data="authAboutProjects.toCheck" :empty-text="emptyText.opens">
+                <el-table-column type="index" label="序号" :index="1" width="100px"></el-table-column>
+                <el-table-column
+                  v-for="item in authedTable"
+                  :key="item.projectName"
+                  :prop="item.prop"
+                  :label="item.label"
+                ></el-table-column>
+                <el-table-column label="操作" fixed="right">
+                  <template slot-scope="scope">
+                    <el-button type="primary" size="mini" @click="toDetail(scope.row._id)">查看</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </el-tab-pane>
+
+            <!-- 用户中心标签页 -->
+            <el-tab-pane label="用户中心" name="fourth">
               <Header :header="centerHeader"></Header>
               <User :userInfo="userInfo"></User>
             </el-tab-pane>
@@ -69,21 +134,61 @@
         </el-col>
       </el-row>
     </el-main>
+
+    <!-- footer部分 -->
     <siteFooter></siteFooter>
   </div>
 </template>
 
 <script>
 import { mapState, mapMutations } from "vuex";
-// import { mapMutations } from "vuex";
-//导入Header，在标签页面内作为页面标题使用
+//导入Header，在标签页面内作为页面标题使用；导入user标签作为用户资料卡
 import Header from "../../components/HeaderinTab.vue";
 import User from "../../components/User.vue";
-//mockdata
-import userInfo from "../../mockData/userInfo";
+//mockData
+import authAboutProjects from "../../mockData/authAboutProjects.js";
 export default {
   data() {
     return {
+      //表格内容为空时的占位符
+      emptyText: {
+        myprojects: `您还没有创建任何项目。`,
+        requests: "没有正在进行的请求。",
+        viewables: "您还没有可查看的开放项目。",
+        checks: "暂无任何授权请求。",
+        opens: "您还没有开放任何项目。"
+      },
+      //用户项目表格
+      projrctsTable: [
+        { prop: "projectName", label: "项目名称" },
+        { prop: "date", label: "创建日期" },
+        { prop: "address", label: "项目地址" }
+      ],
+      //用户请求授权表格
+      toAuthTable: [
+        { prop: "projectName", label: "项目名称" },
+        { prop: "project_id", label: "项目ID" },
+        { prop: "date", label: "申请日期" }
+      ],
+      //用户已获得授权表格
+      viewablesTable: [
+        { prop: "projectName", label: "项目名称" },
+        { prop: "project_id", label: "项目ID" },
+        { prop: "address", label: "项目地址" }
+      ],
+      //等待用户授权表格
+      authTable: [
+        { prop: "projectName", label: "项目名称" },
+        { prop: "userName", label: "申请人" },
+        { prop: "address", label: "申请日期" }
+      ],
+      //用户已开放授权表格
+      authedTable: [
+        { prop: "projectName", label: "项目名称" },
+        { prop: "date", label: "创建日期" },
+        { prop: "address", label: "项目地址" }
+      ],
+      //标签页内用户项目部分header
       projectsHeader: {
         title: "项目列表 ",
         todo: "新建项目",
@@ -94,8 +199,17 @@ export default {
         message:
           "1.表中所列项目为您曾建立的项目。<br />2.点击<strong>新建项目</strong>即可以新建项目。<br />3.点击<strong>详情</strong>即查看对应项目的详细信息。"
       },
+      //标签页内用户正在申请项目部分header
+      requestHeader: {
+        title: "申请列表 ",
+        todo: "申请记录",
+        url: "/history",
+        icon: "el-icon-view",
+        hasQuestion: false
+      },
+      //标签页内用户已获得授权项目部分header
       viewableHeader: {
-        title: "项目列表 ",
+        title: "授权项目列表 ",
         todo: "开放项目",
         url: "/list",
         icon: "el-icon-folder-opened",
@@ -104,10 +218,27 @@ export default {
         message:
           "1.表中展示项目为得到授权可查看的项目。<br />2.点击<strong>开放项目</strong>可以查看所有开放的项目。<br />3.在开放项目获取授权后即可查看项目的详细信息。"
       },
+      //标签页内等待用户授权项目部分header
+      openRequestHeader: {
+        title: "请求列表 ",
+        todo: "所有请求",
+        url: "/auth",
+        icon: "el-icon-coordinate",
+        hasQuestion: false
+      },
+      //标签页内用户已已开放授权项目部分header
+      openHeader: {
+        title: "开放项目列表 ",
+        todo: "我的开放",
+        url: "/list",
+        icon: "el-icon-tickets",
+        hasQuestion: false
+      },
+      //标签页内用户资料卡部分header
       centerHeader: {
         title: "用户资料 ",
         todo: "编辑资料",
-        url: "", //登录时获取用户信息，将URL改为'/user/${id}'
+        url: "/center",
         icon: "el-icon-user",
         hasQuestion: false
       }
@@ -118,10 +249,10 @@ export default {
     User
   },
   computed: {
-    ...mapState(["userInfo"])
+    ...mapState(["userInfo", "loadingFlags", "authAboutProjects"])
   },
   methods: {
-    ...mapMutations(["initUserInfo"]),
+    ...mapMutations(["initAuthAboutProjects", "changeLoading"]),
     toDetail(id) {
       this.$router.push(`/project/${id}/index`);
     },
@@ -131,6 +262,12 @@ export default {
     toAllProjects() {
       this.$router.push("/list");
     },
+    toApplicationDetail(id) {
+      this.$router.push(`/application/${id}`);
+    },
+    toAuthDetail(id) {
+      this.$router.push(`/auth/${id}`);
+    },
     logout() {
       this.$router.push("/index");
     },
@@ -138,9 +275,7 @@ export default {
       this.$store.commit("closeWelcome");
     },
     init() {
-      //异步获取用户数据userInfo；暂时使用mock数据
-      this.initUserInfo(userInfo);
-      this.centerHeader.url = "/center/" + this.userInfo._id;
+      this.initAuthAboutProjects(authAboutProjects);
     }
   },
   created() {
@@ -162,6 +297,9 @@ export default {
 .float-right {
   float: right;
   font-size: 14px;
+}
+.top-margin {
+  margin-top: 80px;
 }
 .welcome {
   font-size: 14px;
