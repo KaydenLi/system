ec9f7b1cb7cce5c38c26c4f603a6221346ada40a
kaydenli <kaydenli@qq.com>
2020-02-27 19:08:51 +0800

    增加了用户授权列表页面

---
+++ src/views/home/myOpenList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <siteHeader :header="header"></siteHeader>
+    <el-main></el-main>
+    <siteFooter></siteFooter>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: { menu: "我的开放列表", toPageName: "返回首页", to: "/" }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
