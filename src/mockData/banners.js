b92155318f15457a5faff9ebc5d4117951613c1f
kaydenli <kaydenli@qq.com>
2020-02-27 19:07:34 +0800

    增加一些mock数据

---
+++ src/mockData/banners.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        _id: "1",
+        url: "https://s.cn.bing.net/th?id=OJ.LXzpAhT0qmRc5g&w=75&h=75&c=8&dpr=1.3&pid=MSNJVFeeds",
+        title: "图片11111",
+    },
+    {
+        _id: "2",
+        url: "https://s.cn.bing.net/th?id=OJ.7WeufAV0ZfB3Rg&w=75&h=75&dpr=1.3&pid=MSNJVFeeds",
+        title: "图片111112222222222",
+    },
+    {
+        _id: "3",
+        url: "https://s.cn.bing.net/th?id=OJ.YTgK7Hy1twKGAw&w=75&h=75&c=8&dpr=1.3&pid=MSNJVFeeds",
+        title: "图片111113333333333",
+    },
+    {
+        _id: "4",
+        url: "https://s.cn.bing.net/th?id=OJ.wSXcWsg0aYz09w&w=75&h=75&dpr=1.3&pid=MSNJVFeeds",
+        title: "图片111114444444",
+    }
+    , {
+        _id: "5",
+        url: "https://s.cn.bing.net/th?id=OJ.AmokZtr02cRjvA&w=75&h=75&dpr=1.3&pid=MSNJVFeeds",
+        title: "图片111115555555555",
+    }
+]
---
+++ src/mockData/links.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        title: "vue",
+        url: "vue.org",
+        _id: "1"
+    },
+    {
+        title: "node",
+        url: "cn.bing.com",
+        _id: "2"
+    },
+    {
+        title: "thingJs",
+        url: "thingjs.com",
+        _id: "3"
+    }]
