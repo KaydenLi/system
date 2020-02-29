e619d9f173e07909baf9a575a30f725faa5db947
kaydenli <kaydenli@qq.com>
2020-02-29 23:06:11 +0800

    增加了用户信息展示页面

---
+++ src/views/home/UserShow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <siteHeader :header="header"></siteHeader>
+    <el-main></el-main>
+    <siteFooter></siteFooter>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: { menu: "查看用户", toPageName: "返回首页", to: "/" }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
