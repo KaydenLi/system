e667cc72aeb063cfb43df156514113702bc75cf1
kaydenli <kaydenli@qq.com>
2020-02-29 23:20:30 +0800

    增加了首页tab活动页面状态，使得返回时在原来tab页面，而不是第一个tab页面

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,36 @@ import getters from './getters.js'
 
 Vue.use(Vuex)
 const state = {
-    userInfo: null, //用户信息
-    authAboutProjects: null,
-    posts:[]
+    userInfo: { //用户信息
+        _id: "",
+        adminFlag: false,
+        loginStatus: false,
+        welcomeFlag: true,
+        userName: "user",
+        address: "cn",
+        phone: "157********",
+        email: "********@**.com",
+        projectsCount: 0,
+        viewablesCount: 0,
+        avatar: "",
+        projects: []
+    },
+    authProjects: {
+        _id: "",
+        userName: "",
+        getChecked: [],
+        toCheck: [],
+        updated: false
+    },
+    applicationProjects: {
+        _id: "",
+        userName: "",
+        getCheckedAuthed: [],
+        toQuest: [],
+        updated: false
+    },
+    posts: [],
+    activeTab: "first"
 }
 
 export default new Vuex.Store({
