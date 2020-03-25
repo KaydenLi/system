be9c4cc2f60c32deccc7e9ca571120bcb750bec2
kaydenli <820807451@qq.com>
2020-03-25 14:05:36 +0800

    修改数据

--- src/mockData/plane.js
+++ src/mockData/plane.js
@@ -9,6 +9,7 @@ import {
 
 export const planes = [
     {
+        _id:"1",
         name: "plane1",
         img: "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
         currenttime: "2020-02-02 20:20:00",
@@ -16,8 +17,9 @@ export const planes = [
         children: [site1, site2, site3]
     },
     {
+        _id:"2",
         name: "plane2",
-        img: "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
+        img: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
         currenttime: "2020-02-02 20:20:00",
         timeStamp: "60",
         children: [site4, site5, site6]
