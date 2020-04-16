eead824e7ff9677f1206af7b6f7208f8ba94e922
kaydenli <820807451@qq.com>
2020-04-16 17:12:13 +0800

    新增threeDatas存储，3D模型测点的数据

--- src/store/index.js
+++ src/store/index.js
@@ -16,8 +16,6 @@ const state = {
         // address: "cn",
         // phone: "157********",
         // email: "********@**.com",
-        // projectsCount: 0,
-        // viewablesCount: 0,
         // avatar: "",
     },
     projects: [//用户项目
@@ -48,37 +46,39 @@ const state = {
     activeTab: "first",//默认活动tab页
     toogleSideBar: false,//隐藏/显示侧边栏项目中心
     project: [],
-    projectInfo: {}
-    // {
-    // _id: "1",
-    // projectName: "湖北省武汉市洪山区华中科技大学xxxxxx建设项目",
-    // ownerId: "1",
-    // createdTime: "2019-09-08",
-    // address: "湖北省武汉市洪山区华中科技大学湖北省武汉市洪山区华中科技大学",
-    // img:
-    // "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
-    // planeId: "",
-    // structuralType: "钢筋混凝土结构，框架结构",
-    // baseInfo: [
-    //   { name: "ID", icon: "el-icon-bank-card", value: "1", unit: "" },
-    //   {
-    //     name: "创建日期",
-    //     icon: "el-icon-date",
-    //     value: "2020/02/02",
-    //     unit: ""
-    //   },
-    //   { name: "查看人数", icon: "el-icon-view", value: 1234, unit: "人" },
-    //   { name: "温度", icon: "el-icon-sunny", value: 25, unit: "℃" },
-    //   { name: "气压", icon: "el-icon-timer", value: 1.0, unit: "MPa" },
-    //   { name: "风速", icon: "el-icon-wind-power", value: 3.5, unit: "m/s" },
-    //   {
-    //     name: "风向",
-    //     icon: "el-icon-wind-power",
-    //     value: "西偏北15",
-    //     unit: "°"
-    //   }
-    // ]
-    // },//当前打开项目详细信息
+    projectInfo: {
+        // {
+        // _id: "1",
+        // projectName: "湖北省武汉市洪山区华中科技大学xxxxxx建设项目",
+        // ownerId: "1",
+        // createdTime: "2019-09-08",
+        // address: "湖北省武汉市洪山区华中科技大学湖北省武汉市洪山区华中科技大学",
+        // img:
+        // "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
+        // planeId: "",
+        // structuralType: "钢筋混凝土结构，框架结构",
+        // baseInfo: [
+        //   { name: "ID", icon: "el-icon-bank-card", value: "1", unit: "" },
+        //   {
+        //     name: "创建日期",
+        //     icon: "el-icon-date",
+        //     value: "2020/02/02",
+        //     unit: ""
+        //   },
+        //   { name: "查看人数", icon: "el-icon-view", value: 1234, unit: "人" },
+        //   { name: "温度", icon: "el-icon-sunny", value: 25, unit: "℃" },
+        //   { name: "气压", icon: "el-icon-timer", value: 1.0, unit: "MPa" },
+        //   { name: "风速", icon: "el-icon-wind-power", value: 3.5, unit: "m/s" },
+        //   {
+        //     name: "风向",
+        //     icon: "el-icon-wind-power",
+        //     value: "西偏北15",
+        //     unit: "°"
+        //   }
+        // ]
+        // },//当前打开项目详细信息
+    },
+    threeDatas: [],
 }
 
 export default new Vuex.Store({
